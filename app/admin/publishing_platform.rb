7ea01ae8e2d97f63cb4db6f88650843494be4d29
Prelang Builder <builder@prelang.com>
2016-12-12 15:57:29 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'PublishingPlatform' to allow for create, update

--- app/admin/publishing_platform.rb
+++ app/admin/publishing_platform.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register PublishingPlatform do
+  permit_params :name, :subscriber_count, :sponsorship_rate, :notes
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
