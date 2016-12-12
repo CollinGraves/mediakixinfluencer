5010fc3170c3fee125668a7f148310c2696f93d0
Prelang Builder <builder@prelang.com>
2016-12-12 15:57:37 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Influencer' to allow for create, update

--- app/admin/influencer.rb
+++ app/admin/influencer.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Influencer do
+  permit_params :first_name, :last_name, :email, :phone
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
