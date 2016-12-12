1eb96fe141767133b80393280fb3a9c943c43a4b
Prelang Builder <builder@prelang.com>
2016-12-12 15:57:25 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'PlatformCampaign' to allow for create, update

--- app/admin/platform_campaign.rb
+++ app/admin/platform_campaign.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register PlatformCampaign do
+  permit_params :name, :contract_num
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
