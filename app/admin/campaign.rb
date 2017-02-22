07970e473bdc6c59205afd7889562655de09a086
José Corcuera <jzcorcuera@gmail.com>
2017-02-22 13:58:18 -0500

    Filter for Brands. #3

--- app/admin/campaign.rb
+++ app/admin/campaign.rb
@@ -2,4 +2,10 @@ ActiveAdmin.register Campaign do
   permit_params :contract_num,
                 :campaign_name
 
+  filter :brand_id, as: :numeric, label: "Brand ID"
+  filter :contract_num, as: :string
+  filter :campaign_name, as: :string
+  filter :created_at
+  filter :updated_at
+
 end
