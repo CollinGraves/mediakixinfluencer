71143ddf2e63a42f90a27d9de89b1dd4f567336c
José Corcuera <jzcorcuera@gmail.com>
2017-02-21 19:18:14 -0500

    Filter Publishing Platforms. #3

--- app/admin/publishing_platform.rb
+++ app/admin/publishing_platform.rb
@@ -1,18 +1,40 @@
 ActiveAdmin.register PublishingPlatform do
-  permit_params :name, :subscriber_count, :sponsorship_rate, :notes, :type
+  permit_params :name,
+    :subscriber_count,
+    :sponsorship_rate,
+    :notes,
+    :type,
+    :url,
+    :monthly_posts,
+    :monthly_page_views,
+    :monthly_unique_visitor,
+    :avg_views,
+    :rate_dedicated,
+    :rate_integrated,
+    :followers_count,
+    :handle,
+    :engage_rate,
+    :avg_opens,
+    :avg_screenshots
 
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
 
+  filter :influencers_id, as: :numeric, label: "Influencer ID"
 
+  filter :type, as: :select, collection: PublishingPlatform::PLATFORMS.map {|platform| [platform.titleize, "#{platform.titleize}PublishingPlatform"] }
+  filter :id, as: :numeric, label: "Publishing Platform ID"
+  filter :url, as: :string
+  filter :handle, as: :string
+  filter :subscriber_count, as: :numeric
+  filter :sponsorship_rate, as: :numeric
+  filter :monthly_posts, as: :numeric
+  filter :monthly_page_views, as: :numeric
+  filter :avg_views, as: :numeric
+  filter :rate_dedicated, as: :numeric
+  filter :rate_integrated, as: :numeric
+  filter :followers_count, as: :numeric
+  filter :engage_rate, as: :numeric
+  filter :avg_opens, as: :numeric
+  filter :avg_screenshots, as: :numeric
+  filter :created_at
+  filter :updated_at
 end
